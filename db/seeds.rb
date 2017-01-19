e676538d311b3f78fa68cd7b8300d7f3116ababf
vscalzo000 <vscalzo000@gmail.com>
2017-01-18 20:47:44 -0500

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,109 +39,109 @@
 #   verified: true
 # )
 #
-# Day.create(name: "Monday")
-# Day.create(name: "Tuesday")
-# Day.create(name: "Wednesday")
-# Day.create(name: "Thursday")
-# Day.create(name: "Friday")
-# Day.create(name: "Saturday")
-# Day.create(name: "Sunday")
+Day.create(name: "Monday")
+Day.create(name: "Tuesday")
+Day.create(name: "Wednesday")
+Day.create(name: "Thursday")
+Day.create(name: "Friday")
+Day.create(name: "Saturday")
+Day.create(name: "Sunday")
 
-# Bar.create(
-#   name: 'Limelight Studios Main Stage',
-#   day: 'Thursday',
-#   time: '8pm-2am',
-#   address: '204 Tremont Street, Boston, MA 02116',
-#   website: 'http://www.limelightboston.com/',
-#   details: 'Limelight’s Stage is a karaoke club, where you rock out in front of a live audience! The stage is usually open on Thursday, Friday & Saturday night. A DJ will host the show and if you want to sing, all you have to do is select your track from the touch-screen kiosks! But, the stage still can be opened for the special party. There will be an admission depends on the event on Thursday, Friday & Saturday night. However, it is FREE for studio customers who have booked their studio in advance. Reservations are not required but are recommended to ensure availability.',
-#   cover_charge: true,
-#   verified: true
-# )
-#
-# Bar.create(
-#   name: 'Limelight Studios Main Stage',
-#   day: 'Friday',
-#   time: '7pm-2am',
-#   address: '204 Tremont Street, Boston, MA 02116',
-#   website: 'http://www.limelightboston.com/',
-#   details: 'Limelight’s Stage is a karaoke club, where you rock out in front of a live audience! The stage is usually open on Thursday, Friday & Saturday night. A DJ will host the show and if you want to sing, all you have to do is select your track from the touch-screen kiosks! But, the stage still can be opened for the special party. There will be an admission depends on the event on Thursday, Friday & Saturday night. However, it is FREE for studio customers who have booked their studio in advance. Reservations are not required but are recommended to ensure availability.',
-#   cover_charge: true,
-#   verified: true
-# )
-#
-# Bar.create(
-#   name: 'Limelight Studios Main Stage',
-#   day: 'Saturday',
-#   time: '7pm-2am',
-#   address: '204 Tremont Street, Boston, MA 02116',
-#   website: 'http://www.limelightboston.com/',
-#   details: 'Limelight’s Stage is a karaoke club, where you rock out in front of a live audience! The stage is usually open on Thursday, Friday & Saturday night. A DJ will host the show and if you want to sing, all you have to do is select your track from the touch-screen kiosks! But, the stage still can be opened for the special party. There will be an admission depends on the event on Thursday, Friday & Saturday night. However, it is FREE for studio customers who have booked their studio in advance. Reservations are not required but are recommended to ensure availability.',
-#   cover_charge: true,
-#   verified: true
-# )
-#
-# Bar.create(
-#   name: 'Bell in Hand',
-#   day: 'Tuesday',
-#   time: '9:30pm-1:30am',
-#   address: '45 Union Street, Boston, MA 02108',
-#   website: 'http://bellinhand.com/',
-#   details: 'Karaoke with DJ P',
-#   cover_charge: false,
-#   verified: true
-# )
-#
-# Bar.create(
-#   name: 'Machine Boston',
-#   day: 'Thursday',
-#   time: '10pm',
-#   address: '1256 Boylston Street, Boston, MA 02215',
-#   website: 'http://www.machineboston.club/mr/home.asp',
-#   details: "Emphysema Menthol 100's is your Karaoke Host. Rob Botelho serves fresh & delicious cocktails.",
-#   cover_charge: false,
-#   verified: true
-# )
-#
-# Bar.create(
-#   name: 'Courtside Karaoke',
-#   day: 'Thursday',
-#   time: '9pm-1am',
-#   address: '291 Cambridge Street, Cambridge, MA 02141',
-#   website: 'http://courtside.wixsite.com/karaoke/facilities',
-#   details: 'The best night in town. Every thursday friday and saturday from 9pm to 1am.',
-#   cover_charge: false,
-#   verified: false
-# )
-#
-# Bar.create(
-#   name: 'Courtside Karaoke',
-#   day: 'Friday',
-#   time: '9pm-1am',
-#   address: '291 Cambridge Street, Cambridge, MA 02141',
-#   website: 'http://courtside.wixsite.com/karaoke/facilities',
-#   details: 'The best night in town. Every thursday friday and saturday from 9pm to 1am.',
-#   cover_charge: false,
-#   verified: false
-# )
-#
-# Bar.create(
-#   name: 'Courtside Karaoke',
-#   day: 'Saturday',
-#   time: '9pm-1am',
-#   address: '291 Cambridge Street, Cambridge, MA 02141',
-#   website: 'http://courtside.wixsite.com/karaoke/facilities',
-#   details: 'The best night in town. Every thursday friday and saturday from 9pm to 1am.',
-#   cover_charge: false,
-#   verified: false
-# )
-#
-# Bar.create(
-#   name: 'The Jeanie Johnston',
-#   day: 'Saturday',
-#   time: '10pm-2am',
-#   address: '144 South Street, Jamaica Plain, MA 02130',
-#   website: 'http://www.jeaniejohnstonjp.com/',
-#   details: 'The low-key neighborhood bar offers a wide variety of weekly entertainment for our countless guests. Such as: Friday night live music, featuring the best local bands. Saturday night Best of Boston winning karaoke. As well as: Geeks Who Drink Trivia night, Fun & Games night and we host a weekly Dart League.',
-#   cover_charge: true,
-#   verified: true
-# )
+Bar.create(
+  name: 'Limelight Studios Main Stage',
+  day: 'Thursday',
+  time: '8pm-2am',
+  address: '204 Tremont Street, Boston, MA 02116',
+  website: 'http://www.limelightboston.com/',
+  details: 'Limelight’s Stage is a karaoke club, where you rock out in front of a live audience! The stage is usually open on Thursday, Friday & Saturday night. A DJ will host the show and if you want to sing, all you have to do is select your track from the touch-screen kiosks! But, the stage still can be opened for the special party. There will be an admission depends on the event on Thursday, Friday & Saturday night. However, it is FREE for studio customers who have booked their studio in advance. Reservations are not required but are recommended to ensure availability.',
+  cover_charge: true,
+  verified: true
+)
+
+Bar.create(
+  name: 'Limelight Studios Main Stage',
+  day: 'Friday',
+  time: '7pm-2am',
+  address: '204 Tremont Street, Boston, MA 02116',
+  website: 'http://www.limelightboston.com/',
+  details: 'Limelight’s Stage is a karaoke club, where you rock out in front of a live audience! The stage is usually open on Thursday, Friday & Saturday night. A DJ will host the show and if you want to sing, all you have to do is select your track from the touch-screen kiosks! But, the stage still can be opened for the special party. There will be an admission depends on the event on Thursday, Friday & Saturday night. However, it is FREE for studio customers who have booked their studio in advance. Reservations are not required but are recommended to ensure availability.',
+  cover_charge: true,
+  verified: true
+)
+
+Bar.create(
+  name: 'Limelight Studios Main Stage',
+  day: 'Saturday',
+  time: '7pm-2am',
+  address: '204 Tremont Street, Boston, MA 02116',
+  website: 'http://www.limelightboston.com/',
+  details: 'Limelight’s Stage is a karaoke club, where you rock out in front of a live audience! The stage is usually open on Thursday, Friday & Saturday night. A DJ will host the show and if you want to sing, all you have to do is select your track from the touch-screen kiosks! But, the stage still can be opened for the special party. There will be an admission depends on the event on Thursday, Friday & Saturday night. However, it is FREE for studio customers who have booked their studio in advance. Reservations are not required but are recommended to ensure availability.',
+  cover_charge: true,
+  verified: true
+)
+
+Bar.create(
+  name: 'Bell in Hand',
+  day: 'Tuesday',
+  time: '9:30pm-1:30am',
+  address: '45 Union Street, Boston, MA 02108',
+  website: 'http://bellinhand.com/',
+  details: 'Karaoke with DJ P',
+  cover_charge: false,
+  verified: true
+)
+
+Bar.create(
+  name: 'Machine Boston',
+  day: 'Thursday',
+  time: '10pm',
+  address: '1256 Boylston Street, Boston, MA 02215',
+  website: 'http://www.machineboston.club/mr/home.asp',
+  details: "Emphysema Menthol 100's is your Karaoke Host. Rob Botelho serves fresh & delicious cocktails.",
+  cover_charge: false,
+  verified: true
+)
+
+Bar.create(
+  name: 'Courtside Karaoke',
+  day: 'Thursday',
+  time: '9pm-1am',
+  address: '291 Cambridge Street, Cambridge, MA 02141',
+  website: 'http://courtside.wixsite.com/karaoke/facilities',
+  details: 'The best night in town. Every thursday friday and saturday from 9pm to 1am.',
+  cover_charge: false,
+  verified: false
+)
+
+Bar.create(
+  name: 'Courtside Karaoke',
+  day: 'Friday',
+  time: '9pm-1am',
+  address: '291 Cambridge Street, Cambridge, MA 02141',
+  website: 'http://courtside.wixsite.com/karaoke/facilities',
+  details: 'The best night in town. Every thursday friday and saturday from 9pm to 1am.',
+  cover_charge: false,
+  verified: false
+)
+
+Bar.create(
+  name: 'Courtside Karaoke',
+  day: 'Saturday',
+  time: '9pm-1am',
+  address: '291 Cambridge Street, Cambridge, MA 02141',
+  website: 'http://courtside.wixsite.com/karaoke/facilities',
+  details: 'The best night in town. Every thursday friday and saturday from 9pm to 1am.',
+  cover_charge: false,
+  verified: false
+)
+
+Bar.create(
+  name: 'The Jeanie Johnston',
+  day: 'Saturday',
+  time: '10pm-2am',
+  address: '144 South Street, Jamaica Plain, MA 02130',
+  website: 'http://www.jeaniejohnstonjp.com/',
+  details: 'The low-key neighborhood bar offers a wide variety of weekly entertainment for our countless guests. Such as: Friday night live music, featuring the best local bands. Saturday night Best of Boston winning karaoke. As well as: Geeks Who Drink Trivia night, Fun & Games night and we host a weekly Dart League.',
+  cover_charge: true,
+  verified: true
+)
